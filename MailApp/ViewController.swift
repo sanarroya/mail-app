403f6ecea2f5eb4248bab2497ad9bf5c5de92e1a
Avila, Santiago X,-ND <Santiago.X.Avila.-ND@Disney.com>
2019-02-21 10:54:19 -0500

    Initial Commit

---
+++ MailApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MailApp
+//
+//  Created by Santiago Avila Arroyave on 2/21/19.
+//  Copyright © 2019 Santiago Avila Arroyave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
